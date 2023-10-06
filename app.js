e990607d89488cafcfac0e07fef6504ddb49c77b
Rico Putra Anugerah <93566840+ricogann@users.noreply.github.com>
2023-10-06 20:58:32 +0700

    testing socket io

--- app.js
+++ app.js
@@ -3,12 +3,29 @@ const cors = require("cors");
 const app = express();
 const routes = require("./routes");
 const bodyParser = require("body-parser");
+const http = require("http");
+const server = http.createServer(app);
 
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "*",
+    },
+});
+
+const socket = io.on("connection", (socket) => {
+    console.log("connect");
+
+    socket.on("disconnect", () => {
+        console.log("server disconnect");
+    });
+    return socket;
+});
 
 app.get("/", (req, res) => {
     res.status(200).json({
@@ -19,6 +36,8 @@ app.get("/", (req, res) => {
 routes(app);
 
 app.use("/assets", express.static(`./public`));
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is running on port http://localhost:${port}`);
 });
+
+export { socket, io };
