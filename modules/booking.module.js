8593f9cb39dae286e9262ae8ac36c6ce9004a4f6
Rico Putra Anugerah <93566840+ricogann@users.noreply.github.com>
2023-10-09 09:56:26 +0700

    test

--- modules/booking.module.js
+++ modules/booking.module.js
@@ -1,7 +1,7 @@
 const Joi = require("joi");
 const prisma = require("../helpers/database");
 const fs = require("fs");
-const { socket } = require("../app");
+const { socket } = require("../app").socket;
 
 class _booking {
     addBooking = async (body, files) => {
