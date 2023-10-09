c5b4707e4def32059112881e540fcd0d63683502
Rico Putra Anugerah <93566840+ricogann@users.noreply.github.com>
2023-10-09 10:40:05 +0700

    test

--- modules/booking.module.js
+++ modules/booking.module.js
@@ -1,7 +1,7 @@
 const Joi = require("joi");
 const prisma = require("../helpers/database");
 const fs = require("fs");
-const { initIo } = require("../app");
+const initIo = require("../app");
 
 const testing = async () => {
     try {
