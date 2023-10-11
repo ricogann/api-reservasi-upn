9c772ce34eab7b7a536d6452fb83e1b33ed5a128
Alfian Dorif Murtadlo1234 <100700571+IndyJones123@users.noreply.github.com>
2023-10-11 23:01:09 +0700

    tesst

--- routes.js
+++ routes.js
@@ -8,15 +8,15 @@ const campusController = require("./controllers/campus.controller");
 const hargaController = require("./controllers/harga.controller");
 const miscController = require("./controllers/misc.controller");
 
-const authorization = require('./middlewares/authorization')
+const authorization = require('./middlewares/authorization');
 
 const _routes = [
     ["auth", authController],
     ["seeder", seederController],
-    ["fasilitas", authorization, fasilitasController],
-    ["kamar", authorization, kamarController],
-    ["users", authorization, usersController],
-    ["booking", authorization, bookingController],
+    ["fasilitas", fasilitasController],
+    ["kamar", kamarController],
+    ["users", usersController],
+    ["booking", bookingController],
     ["campus", campusController],
     ["harga", hargaController],
     ["misc", miscController],
@@ -24,14 +24,14 @@ const _routes = [
 
 const routes = (app) => {
     _routes.forEach((route) => {
-        const [url, controller] = route;
+        const [url, ...controllers] = route;
 
-        const isAuthorizationRequired = ['fasilitas','campus','kamar', 'booking', 'harga'].includes(url);
-        
+        const isAuthorizationRequired = ['fasilitas', 'campus', 'kamar', 'booking', 'harga'].includes(url);
+
         if (isAuthorizationRequired) {
-            app.use(`/api/${url}`, authorization, controller);
+            app.use(`/api/${url}`, authorization, ...controllers);
         } else {
-            app.use(`/api/${url}`, controller);
+            app.use(`/api/${url}`, ...controllers);
         }
     });
 };
