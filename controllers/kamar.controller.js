3fe201ea484b476c3bc6994f819259f1f2437472
Rico Putra Anugerah <93566840+ricogann@users.noreply.github.com>
2023-10-15 15:33:01 +0700

    fix authorization

--- controllers/kamar.controller.js
+++ controllers/kamar.controller.js
@@ -1,36 +1,36 @@
 const m$kamar = require("../modules/kamar.module");
 const response = require("../helpers/response");
+const authorization = require("../middlewares/authorization");
 
 const { Router } = require("express");
 
 const kamarController = Router();
 
-kamarController.post("/add", async (req, res) => {
-    console.log(req.body);
+kamarController.post("/add", authorization, async (req, res) => {
     const result = await m$kamar.addKamar(req.body);
 
     return response.sendResponse(res, result);
 });
 
-kamarController.get("/", async (req, res) => {
+kamarController.get("/", authorization, async (req, res) => {
     const result = await m$kamar.getKamar();
 
     return response.sendResponse(res, result);
 });
 
-kamarController.get("/:id", async (req, res) => {
+kamarController.get("/:id", authorization, async (req, res) => {
     const result = await m$kamar.getKamarById(req.params.id);
 
     return response.sendResponse(res, result);
 });
 
-kamarController.put("/update/:id", async (req, res) => {
+kamarController.put("/update/:id", authorization, async (req, res) => {
     const result = await m$kamar.updateKamar(req.params.id, req.body);
 
     return response.sendResponse(res, result);
 });
 
-kamarController.delete("/delete/:id", async (req, res) => {
+kamarController.delete("/delete/:id", authorization, async (req, res) => {
     const result = await m$kamar.deleteKamar(req.params.id);
 
     return response.sendResponse(res, result);
