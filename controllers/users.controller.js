a47c2ea0aa5da3a380d5abded30b0d54076d74a8
Rico Putra Anugerah <93566840+ricogann@users.noreply.github.com>
2023-10-15 14:04:38 +0700

    add authorization

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,80 +1,107 @@
 const m$users = require("../modules/users.module");
 const response = require("../helpers/response");
+const authorization = require("../middlewares/authorization");
 
 const { Router } = require("express");
 
 const usersController = Router();
 
-usersController.get("/account", async (req, res) => {
+usersController.get("/account", authorization, async (req, res) => {
     const result = await m$users.getAccount();
 
     return response.sendResponse(res, result);
 });
 
-usersController.get("/umum", async (req, res) => {
+usersController.get("/umum", authorization, async (req, res) => {
     const result = await m$users.getUmum();
 
     return response.sendResponse(res, result);
 });
 
-usersController.get("/mahasiswa", async (req, res) => {
+usersController.get("/mahasiswa", authorization, async (req, res) => {
     const result = await m$users.getMahasiswa();
 
     return response.sendResponse(res, result);
 });
 
-usersController.get("/dosen", async (req, res) => {
+usersController.get("/dosen", authorization, async (req, res) => {
     const result = await m$users.getDosen();
 
     return response.sendResponse(res, result);
 });
 
-usersController.get("/account/:id", async (req, res) => {
+usersController.get("/account/:id", authorization, async (req, res) => {
     const result = await m$users.getAccountById(req.params.id);
 
     return response.sendResponse(res, result);
 });
 
-usersController.put("/account/updateStatus/:id", async (req, res) => {
-    const result = await m$users.updateStatus(req.params.id, req.body);
+usersController.put(
+    "/account/updateStatus/:id",
+    authorization,
+    async (req, res) => {
+        const result = await m$users.updateStatus(req.params.id, req.body);
 
-    return response.sendResponse(res, result);
-});
+        return response.sendResponse(res, result);
+    }
+);
 
-usersController.put("/mahasiswa/updateStatus/:id", async (req, res) => {
-    const result = await m$users.updateStatusMahasiswa(req.params.id, req.body);
+usersController.put(
+    "/mahasiswa/updateStatus/:id",
+    authorization,
+    async (req, res) => {
+        const result = await m$users.updateStatusMahasiswa(
+            req.params.id,
+            req.body
+        );
 
-    return response.sendResponse(res, result);
-});
+        return response.sendResponse(res, result);
+    }
+);
 
-usersController.delete("/umum/delete/:id", async (req, res) => {
+usersController.delete("/umum/delete/:id", authorization, async (req, res) => {
     const result = await m$users.deleteUmum(req.params.id);
 
     return response.sendResponse(res, result);
 });
 
-usersController.delete("/dosen/delete/:id", async (req, res) => {
+usersController.delete("/dosen/delete/:id", authorization, async (req, res) => {
     const result = await m$users.deleteDosen(req.params.id);
 
     return response.sendResponse(res, result);
 });
 
-usersController.delete("/mahasiswa/delete/:id", async (req, res) => {
-    const result = await m$users.deleteMahasiswa(req.params.id);
+usersController.delete(
+    "/mahasiswa/delete/:id",
+    authorization,
+    async (req, res) => {
+        const result = await m$users.deleteMahasiswa(req.params.id);
 
-    return response.sendResponse(res, result);
-});
+        return response.sendResponse(res, result);
+    }
+);
 
-usersController.put("/account/verifikasi/:id", async (req, res) => {
-    const result = await m$users.updateStatusAccount(req.params.id, req.body);
+usersController.put(
+    "/account/verifikasi/:id",
+    authorization,
+    async (req, res) => {
+        const result = await m$users.updateStatusAccount(
+            req.params.id,
+            req.body
+        );
 
-    return response.sendResponse(res, result);
-});
+        return response.sendResponse(res, result);
+    }
+);
 
-usersController.post("/mahasiswa/check/:id", async (req, res) => {
-    const result = await m$users.checkExpiredMahasiswa(req.params.id);
+usersController.post(
+    "/mahasiswa/check/:id",
+    authorization,
+    async (req, res) => {
+        const result = await m$users.checkExpiredMahasiswa(req.params.id);
 
-    return response.sendResponse(res, result);
-});
+        return response.sendResponse(res, result);
+    }
+);
 
 module.exports = usersController;
